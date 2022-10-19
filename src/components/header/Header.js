bec5cdfb182df0396a107aa8e6b5a7646ed2eab4
fabio4520 <fabioleofc@gmail.com>
2022-10-19 18:45:15 -0500

    ADD: react-icons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,14 @@ import React from 'react'
 function Header() {
   return (
     <header>
-      
+      <ul>
+        <li>Products</li>
+        <li>Categories</li>
+      </ul>
+      <ul>
+        <li>Login</li>
+        <li>Singup</li>
+      </ul>
     </header>
   )
 }
