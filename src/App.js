bb95c3a9a9c012f00215c78c3294967f030e4849
fabio4520 <fabioleofc@gmail.com>
2022-10-20 04:06:36 -0500

    ADD: routes for crud operations and categories page Problem - Need to add routes Solution - Add react-router-dom - Add links for buttons Note It needs to be refactorize.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Header from './components/header/Header';
 import EditProduct from './components/products/EditProduct';
 import NewProduct from './components/products/NewProduct';
 import Products from './components/products/Products';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="pr-10 pl-10 pt-4 pb-4 bg-[#eeeeee] h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 font-sans">
       <Header />
       <div className='flex items-center justify-center'>
-        <Products/>
-        {/* <EditProduct id={ 1 } /> */}
-        {/* <Categories/> */}
-        {/* <NewProduct/> */}
+        <Routes>
+          <Route path='/' element={ <Products/>} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/products/:id' element={<EditProduct/>}/>
+          <Route path='/new/product' element={<NewProduct/>}/>
+          <Route path='/categories' element={ <Categories/>} />
+        </Routes>
       </div>
+
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,13 +2,18 @@
 import React from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import Option from './Option'
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-30 xl:items-center'>
       <ul className='flex flex-row gap-5 items-center'>
-        <Option>Products</Option>
-        <Option>Categories</Option>
+        <Link to={'/products'}>
+          <Option>Products</Option>
+        </Link>
+        <Link to={'/categories'}>
+          <Option>Categories</Option>
+        </Link>
       </ul>
       <ul className='flex flex-row gap-5 items-center'>
         <Option>Login</Option>
